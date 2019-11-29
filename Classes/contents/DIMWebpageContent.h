83cc358b34a5a35e4033ff134f057b9ad161bfa4
Moky <albert.moky@gmail.com>
2019-11-29 16:07:09 +0800

    rename dir

---
+++ Classes/contents/DIMWebpageContent.h
@@ -0,0 +1,67 @@
+// license: https://mit-license.org
+//
+//  DIMP : Decentralized Instant Messaging Protocol
+//
+//                               Written in 2018 by Moky <albert.moky@gmail.com>
+//
+// =============================================================================
+// The MIT License (MIT)
+//
+// Copyright (c) 2019 Albert Moky
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
+//
+//  DIMWebpageContent.h
+//  DIMCore
+//
+//  Created by Albert Moky on 2018/11/27.
+//  Copyright © 2018 DIM Group. All rights reserved.
+//
+
+#import "dimMacros.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DIMWebpageContent : DIMContent
+
+@property (readonly, strong, nonatomic) NSURL *URL;
+@property (readonly, strong, nonatomic, nullable) NSString *title;
+@property (readonly, strong, nonatomic, nullable) NSString *desc;
+@property (readonly, strong, nonatomic, nullable) NSData *icon;
+
+/*
+ *  Web Page message: {
+ *      type : 0x20,
+ *      sn   : 123,
+ *
+ *      URL   : "https://github.com/moky/dimp", // Page URL
+ *      icon  : "...",                          // base64_encode(icon)
+ *      title : "...",
+ *      desc  : "..."
+ *  }
+ */
+- (instancetype)initWithURL:(NSURL *)url
+                      title:(nullable NSString *)title
+                description:(nullable NSString *)desc
+                       icon:(nullable NSData *)icon;
+
+@end
+
+NS_ASSUME_NONNULL_END
