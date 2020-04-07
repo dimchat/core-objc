8564492a2ec89f9515940cfa2514dc24ea493ccb
Moky <albert.moky@gmail.com>
2020-04-08 01:03:43 +0800

    DIMP upgraded to v0.4.0

--- DIMCore/DIMCoreTests/DIMCoreTests.m
+++ DIMCore/DIMCoreTests/DIMCoreTests.m
@@ -8,11 +8,6 @@
 
 #import <XCTest/XCTest.h>
 
-#import <DIMCore/DIMCore.h>
-
-#import "NSObject+JsON.h"
-#import "NSData+Extension.h"
-
 @interface DIMCoreTests : XCTestCase
 
 @end
@@ -37,25 +32,6 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
-}
-
-- (void)testHash {
-    
-    NSString *string = @"moky";
-    NSData *data = [string data];
-    
-    NSData *hash;
-    NSString *res;
-    NSString *exp;
-    
-    
-    // md5（moky）= d0e5edd3fd12b89154bbe7a5e4c82569
-    exp = @"d0e5edd3fd12b89154bbe7a5e4c82569";
-    hash = [data md5];
-    res = [hash hexEncode];
-    NSLog(@"md5(%@) = %@", string, res);
-    NSAssert([res isEqual:exp], @"md5 error: %@ != %@", res, exp);
-    
 }
 
 @end
